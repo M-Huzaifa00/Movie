20d9309f1a81b82ee767c56d5a68a02dfdab86b1
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-24 16:52:29 -0700

    Make Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Grid } from "@chakra-ui/react"
+
 function App() {
 
   return (
     <>
+    <Grid templateAreas={
+      `'NavBar' 'NavBar'
+      'Genres' 'Dynamic'
+      'Main''Main'
+      `
+    }
+    templateColumns={{
+      base: '1fr',
+      lg: '250px 1fr'
+    }}
+    >
+
+    </Grid>
     </>
   )
 }
