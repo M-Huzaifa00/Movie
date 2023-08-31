ae58ba32fe4d12beb6fca9406e1aff79c7ddf055
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-31 14:46:17 -0700

    ALL DONE

--- src/Components/MovieDetailPage.tsx
+++ src/Components/MovieDetailPage.tsx
@@ -1,7 +1,9 @@
-import { Box, GridItem, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
+import { Image, Box, GridItem, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import { useMovieDetail } from '../Components/Hooks/useMovieDetail'
 import { DefinationItem } from './DefinationItem';
 import { useMovieVideos } from './Hooks/useMovieVideos';
+import { useMovieImage } from './Hooks/useMovieImage';
+import { imageRequest } from '../Services/imageRequest';
 
 interface Props {
     id: number;
@@ -10,76 +12,80 @@ interface Props {
 export const MovieDetailPage = ({ id }: Props) => {
     const { data: Movie, isLoading, error } = useMovieDetail(id)
     const { data } = useMovieVideos(id)
+    const { data: Images } = useMovieImage(id)
     const result = data?.results.find(({ type, name }) => {
         return type === 'Trailer' && name === 'Official Trailer';
     });
-    console.log(result?.key)
-
     const uniquerCountries = [...new Set(Movie?.production_countries)]
     if (error) throw Error;
     if (isLoading) return <Box marginTop={90} display={'flex'} justifyContent={'center'} ><Spinner /></Box>;
 
 
     return (
-        <SimpleGrid fontFamily={'ubuntu'} paddingX={{ md: 14 }} padding={4} columns={{ base: 1, md: 2 }} >
-            <GridItem >
-                <Heading
-                    color={'#E50914'}
-                >
-                    {Movie?.original_title}
-                </Heading>
-                <Text>
-                    {
-                        Movie?.overview
-                    }
-                </Text>
-                <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} as='dl'>
-                    <DefinationItem term={'Genres'}>
+        <>
+            <SimpleGrid fontFamily={'ubuntu'} paddingX={{ md: 14 }} padding={4} columns={{ base: 1, md: 2 }} >
+                <GridItem >
+                    <Heading
+                        color={'#E50914'}
+                    >
+                        {Movie?.original_title}
+                    </Heading>
+                    <Text>
                         {
-                            Movie?.genres.map(({ id, name }) => <Text key={id} >{name}</Text>)
+                            Movie?.overview
                         }
-                    </DefinationItem>
-                    <DefinationItem term={'Production'}>
-                        {
-                            Movie?.production_companies.map(({ id, name }) => <Text key={id} >{name}</Text>)
-                        }
-                    </DefinationItem>
-                    <DefinationItem term={'Countries'}>
-                        {
-                            uniquerCountries.map(({ name }) => <Text key={name} >{name}</Text>)
-                        }
-                    </DefinationItem>
-                    <DefinationItem term='Status'>
-                        <Text>{Movie?.status}</Text>
-                    </DefinationItem>
-                    <DefinationItem term="Budget">
-                        {
-                            <Text>
-                                {Movie?.budget ? Movie?.budget : 'Unknown'}
-                            </Text>
-                        }
-                    </DefinationItem>
-                    <DefinationItem term="Release Date">
-                        {
-                            <Text>
-                                {Movie?.release_date}
-                            </Text>
-                        }
-                    </DefinationItem>
-                </SimpleGrid>
-            </GridItem>
-            <GridItem display={'flex'} justifyContent={'center'} padding={{ lg: 4 }} >
-                <iframe
-                    style={{ margin: 'auto' }}
-                    src={`https://www.youtube.com/embed/${result?.key}`}
-                    title="Embedded Video"
-                    allowFullScreen
-                    width="100%"  // 
-                    height="300" //
-                ></iframe>
-
-            </GridItem>
-
-        </SimpleGrid>
+                    </Text>
+                    <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} as='dl'>
+                        <DefinationItem term={'Genres'}>
+                            {
+                                Movie?.genres.map(({ id, name }) => <Text key={id} >{name}</Text>)
+                            }
+                        </DefinationItem>
+                        <DefinationItem term={'Production'}>
+                            {
+                                Movie?.production_companies.map(({ id, name }) => <Text key={id} >{name}</Text>)
+                            }
+                        </DefinationItem>
+                        <DefinationItem term={'Countries'}>
+                            {
+                                uniquerCountries.map(({ name }) => <Text key={name} >{name}</Text>)
+                            }
+                        </DefinationItem>
+                        <DefinationItem term='Status'>
+                            <Text>{Movie?.status}</Text>
+                        </DefinationItem>
+                        <DefinationItem term="Budget">
+                            {
+                                <Text>
+                                    {Movie?.budget ? Movie?.budget : 'Unknown'}
+                                </Text>
+                            }
+                        </DefinationItem>
+                        <DefinationItem term="Release Date">
+                            {
+                                <Text>
+                                    {Movie?.release_date}
+                                </Text>
+                            }
+                        </DefinationItem>
+                    </SimpleGrid>
+                </GridItem>
+                <GridItem display={'flex'} justifyContent={'center'} padding={{ lg: 4 }} >
+                    <iframe
+                        style={{ margin: 'auto' }}
+                        src={`https://www.youtube.com/embed/${result?.key}`}
+                        title="Embedded Video"
+                        allowFullScreen
+                        width="100%"  
+                        height="300" 
+                    ></iframe>
+                </GridItem>
+            </SimpleGrid>
+            <SimpleGrid padding={{base:2,md:6}} columns={{base:2 , md:4}} spacing={5}>
+                {
+                    Images?.backdrops.map(({ file_path }) => <Image src={imageRequest(file_path)} />)
+                }
+            </SimpleGrid>
+        </>
     )
 }
